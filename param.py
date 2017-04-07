0f912fa2910d7061b5e7aa445eaba10d20c735ab
Fred Floyd <ffloyd@daktaridx.com>
2017-04-07 09:10:18 -0400

    Changed output for silver/no silver

--- param.py
+++ param.py
@@ -6,7 +6,9 @@
 ##########
 
 #TESTING PARAMETERS
-DispenseV2 = False
+AddSilver = True
+
+DispenseV2 = True
 DispenseFlowrate = 100
 DispenseVolume = 300
 
@@ -65,7 +67,7 @@ WashoutRate100 = 100      #Air flowrate for 100 uL(uL/min)
 WashoutVol100 = 150       #Air Volume for 100 uL (uL)
 PulldownTime1 = 10        #Time for mags to pull down
 WashoutRate50 = 100       #Air flowrate (uL/min)
-WashoutVol50 = 100        #Air volume for 50uL (uL/min)
+WashoutVol50 = 150        #Air volume for 50uL (uL/min)
 
 #WASH PARAMETERS
 WashRate = 100
