d3c92b6e32b76616fe0f9627ff0b7c041d2c32c2
Fred Floyd <ffloyd@daktaridx.com>
2017-04-11 12:14:35 -0400

    updated syringe pump connection

--- SyringePump.py
+++ SyringePump.py
@@ -14,103 +14,104 @@ import DebugFunctions as db
 import serial
 import time
 
+
 class SyringePump:
+    theComPort = ""
+    theConnection = None
 
-	theComPort=""
-	theConnection=None
+    pumpDelay = 0.5  # delay after pump communication
 
-	pumpDelay=0.5 #delay after pump communication
+    def __init__(self, ComPort):
+        self.theComPort = ComPort
 
-	def __init__(self, ComPort):
-		self.theComPort=ComPort
+    def Connect(self):
+        db.PrintDebug("Connecting syringe pump on port " + str(self.theComPort))
+        success = True
 
-	def Connect(self):
-		db.PrintDebug("Connecting syringe pump on port "+str(self.theComPort))
-		success=True
-		
-		try:
-			self.theConnection=serial.Serial(self.theComPort,38400,serial.EIGHTBITS,serial.PARITY_NONE,serial.STOPBITS_ONE,1,False,False, writeTimeout=1)
-		except:
-			db.PrintDebug("No pump connection!")
-			self.theConnection=None
-			success=False
-					
-		return success
-	
-	def Disconnect(self):
-		db.PrintDebug("Disconnecting syringe pump on port "+str(self.theComPort))
-		if (self.theConnection):
-			self.theConnection.close()
+        try:
+            self.theConnection = serial.Serial(self.theComPort, 38400, serial.EIGHTBITS, serial.PARITY_NONE,
+                                               serial.STOPBITS_ONE, 1, False, False, writeTimeout=1)
+        except:
+            db.PrintDebug("No pump connection!")
+            self.theConnection = None
+            success = False
 
-	def Configure(self, diameter):
-		db.PrintDebug("Setting syringe pump on port "+str(self.theComPort)+" to diameter "+str(diameter))
-		if (self.theConnection):
-			self.theConnection.flushInput()
-			self.theConnection.write("set units 2\r\n")
-			db.PrintDebug("pump: " + self.theConnection.readline())
-			time.sleep(self.pumpDelay)
-			self.theConnection.flushInput()
-			self.theConnection.write("set diameter "+str(diameter)+"\r\n")
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			time.sleep(self.pumpDelay)
+        return success
 
-	def SetRate(self, rate):
-		db.PrintDebug("Setting rate syringe pump on port "+str(self.theComPort)+" to "+str(rate))
-		if (self.theConnection):
-			self.theConnection.flushInput()
-			self.theConnection.write("set rate "+str(rate)+"\r\n")
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			time.sleep(self.pumpDelay)
+    def Disconnect(self):
+        db.PrintDebug("Disconnecting syringe pump on port " + str(self.theComPort))
+        if (self.theConnection):
+            self.theConnection.close()
 
+    def Configure(self, diameter):
+        db.PrintDebug("Setting syringe pump on port " + str(self.theComPort) + " to diameter " + str(diameter))
+        if (self.theConnection):
+            self.theConnection.flushInput()
+            self.theConnection.write("set units 2\r\n")
+            db.PrintDebug("Python: " + self.theConnection.readline())
+            db.PrintDebug("Pump: " + self.theConnection.readline())
+            time.sleep(self.pumpDelay)
+            self.theConnection.flushInput()
+            self.theConnection.write("set diameter " + str(diameter) + "\r\n")
+            db.PrintDebug("Python: " + self.theConnection.readline())
+            db.PrintDebug("Pump: " + self.theConnection.readline())
+            time.sleep(self.pumpDelay)
 
-	def SetVolume(self, vol):
-		db.PrintDebug("Setting volume syringe pump on port "+str(self.theComPort)+" to "+str(vol))
-		if (self.theConnection):
-			self.theConnection.flushInput()
-			self.theConnection.write("set volume "+str(vol)+"\r\n")
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			time.sleep(self.pumpDelay)
+    def SetRate(self, rate):
+        db.PrintDebug("Setting rate syringe pump on port " + str(self.theComPort) + " to " + str(rate))
+        if (self.theConnection):
+            self.theConnection.flushInput()
+            self.theConnection.write("set rate " + str(rate) + "\r\n")
+            db.PrintDebug("pump: " + self.theConnection.readline())
+            db.PrintDebug("pump: " + self.theConnection.readline())
+            time.sleep(self.pumpDelay)
 
-	def EnableWithdraw(self):
-		db.PrintDebug("Withdrawing pump on port "+str(self.theComPort))
-		if (self.theConnection):
-			self.theConnection.flushInput()
-			self.theConnection.write("set volume -100\r\n")
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			time.sleep(self.pumpDelay)
+    def SetVolume(self, vol):
+        db.PrintDebug("Setting volume syringe pump on port " + str(self.theComPort) + " to " + str(vol))
+        if (self.theConnection):
+            self.theConnection.flushInput()
+            self.theConnection.write("set volume " + str(vol) + "\r\n")
+            db.PrintDebug("Python: " + self.theConnection.readline())
+            db.PrintDebug("Pump: " + self.theConnection.readline())
+            time.sleep(self.pumpDelay)
 
-	def DisableWithdraw(self):
-		db.PrintDebug("Withdrawing pump on port "+str(self.theComPort))
-		if (self.theConnection):
-			self.theConnection.flushInput()
-			self.theConnection.write("set volume 100\r\n")
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			time.sleep(self.pumpDelay)
+    def EnableWithdraw(self):
+        db.PrintDebug("Withdrawing pump on port " + str(self.theComPort))
+        if (self.theConnection):
+            self.theConnection.flushInput()
+            self.theConnection.write("set volume -100\r\n")
+            db.PrintDebug("pump: " + self.theConnection.readline())
+            db.PrintDebug("pump: " + self.theConnection.readline())
+            db.PrintDebug("pump: " + self.theConnection.readline())
+            time.sleep(self.pumpDelay)
 
-	def Start(self, rate):
-		db.PrintDebug("Starting syringe pump on port "+str(self.theComPort)+" at rate "+str(rate))
-		if (self.theConnection):
-			self.theConnection.flushInput()
-			self.theConnection.write("set rate "+str(rate)+"\r\n")
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			time.sleep(self.pumpDelay)
-			self.theConnection.write("start\r\n")
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			time.sleep(self.pumpDelay)
-		
-	def Stop(self):
-		db.PrintDebug("Stopping syringe pump on port "+str(self.theComPort))
-		if (self.theConnection):
-			self.theConnection.flushInput()
-			self.theConnection.write("stop\r\n")
-			db.PrintDebug("pump: "+self.theConnection.readline())
-			time.sleep(self.pumpDelay)
+    def DisableWithdraw(self):
+        db.PrintDebug("Withdrawing pump on port " + str(self.theComPort))
+        if (self.theConnection):
+            self.theConnection.flushInput()
+            self.theConnection.write("set volume 100\r\n")
+            db.PrintDebug("pump: " + self.theConnection.readline())
+            db.PrintDebug("pump: " + self.theConnection.readline())
+            db.PrintDebug("pump: " + self.theConnection.readline())
+            time.sleep(self.pumpDelay)
+
+    def Start(self, rate):
+        db.PrintDebug("Starting syringe pump on port " + str(self.theComPort) + " at rate " + str(rate))
+        if (self.theConnection):
+            self.theConnection.flushInput()
+            self.theConnection.write("set rate " + str(rate) + "\r\n")
+            db.PrintDebug("pump: " + self.theConnection.readline())
+            db.PrintDebug("pump: " + self.theConnection.readline())
+            time.sleep(self.pumpDelay)
+            self.theConnection.write("start\r\n")
+            db.PrintDebug("pump: " + self.theConnection.readline())
+            db.PrintDebug("pump: " + self.theConnection.readline())
+            time.sleep(self.pumpDelay)
+
+    def Stop(self):
+        db.PrintDebug("Stopping syringe pump on port " + str(self.theComPort))
+        if (self.theConnection):
+            self.theConnection.flushInput()
+            self.theConnection.write("stop\r\n")
+            db.PrintDebug("pump: " + self.theConnection.readline())
+            time.sleep(self.pumpDelay)
